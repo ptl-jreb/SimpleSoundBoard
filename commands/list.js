d4cebe96c1a22540dac24ce46c7cf90791b72c19
dgri <david.grill@interact-iv.com>
2020-09-24 17:35:15 +0200

    Update list.js

--- commands/list.js
+++ commands/list.js
@@ -8,7 +8,7 @@ module.exports = {
     if (!message.guild) return
     const files = fs.readdirSync(path.join(__dirname, '..', 'sounds'))
     let str = ''
-    files.forEach(file => (str = str + str.length ? ', ' : '' + `${file.replace('.mp3', '')}`))
+    files.forEach(file => (str = str + (str.length ? ', ' : '') + `${file.replace('.mp3', '')}`))
     message.reply(`Sounds list: ${str}`)
   }
 }
