0f9d10d56cc63d5bebf34c6cb3b7c035fbe2df11
Clément Delalandre <clement.delalandre@interactiv-group.com>
2020-09-24 16:55:43 +0200

    first commit

---
+++ commands/play.js
@@ -0,0 +1,52 @@
+module.exports = {
+  name: "play",
+  description: "Play requested sound.",
+  async execute(message, args) {
+    if (!message.guild) return;
+
+    if (message.member.voice.channel) {
+      // const ytdl = require('ytdl-core');
+      const connection = await message.member.voice.channel.join();
+      const sound = args[0];
+      let url2play = "";
+      switch (sound) {
+        case "wow":
+          url2play = './../sounds/wow.mp3';
+          break;
+        case "coucou":
+          url2play = './../sounds/coucou.mp3';
+          break;
+        case "zizi":
+          url2play = './../sounds/zizi.mp3';
+          break;
+        case "sad":
+          url2play = './../sounds/sad.mp3';
+          break;
+        default:
+          url2play = './../sounds/nope.mp3';
+      }
+      const dispatcher = connection.play(url2play, {
+        volume: 0.5,
+      });
+
+      dispatcher.on("start", () => {
+        message.client.user.setActivity("SimpleSoundBoard", {
+          type: "LISTENING",
+        });
+      });
+
+      dispatcher.on("error", () => {
+        message.reply("Je n'ai pas réussi à lire cette vidéo !");
+        dispatcher.destroy();
+        message.member.voice.channel.leave();
+      });
+
+      dispatcher.on("finish", () => {
+        dispatcher.destroy();
+        message.member.voice.channel.leave();
+      });
+    } else {
+      message.reply("You need to join a voice channel first!");
+    }
+  },
+};
