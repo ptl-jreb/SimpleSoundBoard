bfdb3c8d45f7a8b7b0ba5b19cf0a882fd731daf9
dgri <david.grill@interact-iv.com>
2020-09-24 17:23:46 +0200

    Test

--- commands/list.js
+++ commands/list.js
@@ -1,9 +1,14 @@
+const fs = require('fs')
+const path = require('path')
+
 module.exports = {
   name: 'list',
   description: 'List sounds.',
   async execute (message, args) {
     if (!message.guild) return
-
-    message.reply('wow coucou zizi sad')
+    const files = fs.readdirSync(path.join(__dirname, '..', 'sounds'))
+    let str = `Sounds list:`
+    files.forEach(file => (str = str + `  -${file.replace('.mp3', '')}`))
+    message.reply(str)
   }
 }
--- commands/play.js
+++ commands/play.js
@@ -1,3 +1,6 @@
+const fs = require('fs')
+const path = require('path')
+
 module.exports = {
   name: 'play',
   description: 'Play requested sound.',
@@ -7,39 +10,25 @@ module.exports = {
     if (message.member.voice.channel) {
       const connection = await message.member.voice.channel.join()
       const sound = args[0]
-      let url2play = ''
-      switch (sound) {
-        case 'wow':
-          url2play = 'http://ssb.digidrive.io/wow.mp3'
-          break
-        case 'coucou':
-          url2play = 'http://ssb.digidrive.io/coucou.mp3'
-          break
-        case 'zizi':
-          url2play = 'http://ssb.digidrive.io/zizi.mp3'
-          break
-        case 'sad':
-          url2play = 'http://ssb.digidrive.io/sad.mp3'
-          break
-        default:
-          url2play = 'http://ssb.digidrive.io/nope.mp3'
+
+      if (!fs.existsSync(path.join(__dirname, '..', 'sounds', sound + '.mp3'))) {
+        message.reply('Sound does not exists !')
+      } else {
+        const url2play = `http://ssb.digidrive.io/${sound}.mp3`
+        const dispatcher = connection.play(url2play, { volume: 1 })
+        dispatcher.on('start', () => {
+          message.client.user.setActivity('SimpleSoundBoard', { type: 'LISTENING' })
+        })
+        dispatcher.on('error', () => {
+          message.reply('Je n\'ai pas réussi à lire cette vidéo !')
+          dispatcher.destroy()
+          message.member.voice.channel.leave()
+        })
+        dispatcher.on('finish', () => {
+          dispatcher.destroy()
+          message.member.voice.channel.leave()
+        })
       }
-      const dispatcher = connection.play(url2play, { volume: 1 })
-
-      dispatcher.on('start', () => {
-        message.client.user.setActivity('SimpleSoundBoard', { type: 'LISTENING' })
-      })
-
-      dispatcher.on('error', () => {
-        message.reply('Je n\'ai pas réussi à lire cette vidéo !')
-        dispatcher.destroy()
-        message.member.voice.channel.leave()
-      })
-
-      dispatcher.on('finish', () => {
-        dispatcher.destroy()
-        message.member.voice.channel.leave()
-      })
     } else {
       message.reply('You need to join a voice channel first!')
     }
