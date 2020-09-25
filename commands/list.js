6c98d95bbab46c81bca27c4e334c44cc5d0d811d
dgri <david.grill@interact-iv.com>
2020-09-25 07:56:27 +0200

    Update list.js

--- commands/list.js
+++ commands/list.js
@@ -7,6 +7,7 @@ module.exports = {
   async execute (message, args) {
     if (!message.guild) return
     const files = fs.readdirSync(path.join(__dirname, '..', 'sounds'))
+    files.sort((a, b) => a.localeCompare(b))
     let str = ''
     files.forEach(file => (str = str + (str.length ? ', ' : '') + `${file.replace('.mp3', '')}`))
     message.reply(`Sounds list: ${str}`)
