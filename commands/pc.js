cf8c347d55e622c985b9075eeef62f3c21dbd244
Swarley1166 <72025653+swarley1166@users.noreply.github.com>
2020-11-14 15:48:32 +0100

    Update pc.js

--- commands/pc.js
+++ commands/pc.js
@@ -10,27 +10,27 @@ module.exports = {
 
     if (message.member.voice.channel) {
       const connection = await message.member.voice.channel.join()
-      if (!args[0]) message.reply('You need to provide a category code!')
+      if (!args[0]) message.channel.send('You need to provide a category code!')
 
       let category = categories[args[0]]
       if (!category) {
-        message.reply('Category does not exists !')
+        message.channel.send('Category does not exists !')
       } else if (category && !category.length) {
-        message.reply('Category does not have sounds !')
+        message.channel.send('Category does not have sounds !')
       } else {
         let sound = (Math.floor(Math.random() * Math.floor(20)) > 18) ? 'rickrolled' : category[Math.floor(Math.random() * category.length)]
 
         if (!fs.existsSync(path.join(__dirname, '..', 'sounds', sound + '.mp3'))) {
-          message.reply('Sound does not exists !')
+          message.channel.send('Sound does not exists !')
         } else {
           const url2play = `http://ssb.digidrive.io/${sound}.mp3`
           const dispatcher = connection.play(url2play, { volume: 1 })
-          if (sound === 'rickrolled') message.reply('You\'ve been RICKROLLED !')
+          if (sound === 'rickrolled') message.channel.send('You\'ve been RICKROLLED !')
           dispatcher.on('start', () => {
             message.client.user.setActivity('SimpleSoundBoard', { type: 'LISTENING' })
           })
           dispatcher.on('error', () => {
-            message.reply('Je n\'ai pas réussi à lire cette vidéo !')
+            message.channel.send('Je n\'ai pas réussi à lire cette vidéo !')
             dispatcher.destroy()
             message.member.voice.channel.leave()
           })
